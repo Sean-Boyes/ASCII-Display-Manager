9fcff641622db69b5b489ba2b2f4b3d40c56e1b9
Sean-Boyes <118757050+Sean-Boyes@users.noreply.github.com>
2023-07-24 16:47:03 -0700

    Update Example.cpp

--- ASCII/Example.cpp
+++ ASCII/Example.cpp
@@ -40,12 +40,15 @@ int main()
 
 	ASCIIobject screen, menu, border;
 	screen.createCanvas(40, 20);
+	screen.paint(getEffect('u'));
 	screen.place();
 
 	menu.createCanvas(20, 5, "~", "|");
+	menu.paint(getColour("white", 0, 1));
 	menu.place(3, 3);
 
 	border.createCanvas(15, 10, "_", "}", "O", "O", "O", "O");
+	border.paint(getColour("blue", 1, 0));
 	border.place(24, 9);
 
 	CsrMoveTo(screen.dimension[0] + 1, screen.dimension[1] + 1);
